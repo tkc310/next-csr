30bc1c442ccc81a50edcfa572f35eac87cf19c58
Takashi Sato <takashisato.235711@gmail.com>
2021-07-26 20:44:57 +0900

    bundle-analizer導入

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+// analize
+// <distDir>/analyze/
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const config = {
   reactStrictMode: true,
-}
+};
+
+module.exports = withBundleAnalyzer(config);
