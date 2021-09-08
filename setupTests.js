725e6a04831eb4ec203c0d410140465b636a5f78
TakashiSato <takashisato.develop@gmail.com>
2021-09-08 16:22:37 +0900

    testを修正

--- setupTests.js
+++ setupTests.js
@@ -1,8 +1,4 @@
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen, cleanup } from '@testing-library/react';
-import { initTestHelpers, getPage } from 'next-page-tester';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
-import userEvent from '@testing-library/user-event';
+import { initTestHelpers } from 'next-page-tester';
 
 initTestHelpers();
--- src/components/pages/Post/index.test.tsx
+++ src/components/pages/Post/index.test.tsx
@@ -1,23 +1,84 @@
+import { screen, act, waitForElementToBeRemoved } from '@testing-library/react';
+import prettier from 'prettier';
 import { getPage } from 'next-page-tester';
-import { screen } from '@testing-library/react';
-import { PostFragment } from '@/graphql/generated/pages/post';
+import { MockedProvider } from '@apollo/client/testing';
+import { PostIndexDocument } from '@/graphql/generated/pages/post';
+
+const post = {
+  __typename: 'Post',
+  id: '1',
+  title: 'titile foo',
+  content: 'content foo',
+};
+
+const mocks = [
+  {
+    request: {
+      query: PostIndexDocument,
+      variables: {
+        id: 1,
+      },
+    },
+    result: {
+      data: {
+        post,
+      },
+    },
+  },
+];
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const getNextPage = async (route: string) => {
+  const pageTester = await getPage({
+    nextRoot: `${process.cwd()}/src`,
+    route,
+    useDocument: true,
+    wrapper: {
+      // eslint-disable-next-line react/display-name
+      Page: (Page) => (pageProps) => {
+        return (
+          <MockedProvider mocks={mocks} addTypename={false}>
+            <Page {...pageProps} />
+          </MockedProvider>
+        );
+      },
+    },
+  });
+
+  return pageTester;
+};
 
 describe('Post', () => {
-  const data: PostFragment = {
-    post: {
-      __typename: 'Post',
-      id: '1',
-      title: 'title foo',
-      content: 'content',
-    },
-  };
-
   it('renders without crashing', async () => {
-    const { render } = await getPage({
-      route: '/',
+    const loadingText = 'Loading';
+
+    // next-page-testerのサンプル
+    // @see https://github.com/toomuchdesign/next-page-tester/blob/master/examples/03-apollo-client.md
+    const { render: nextRender } = await getNextPage('/posts/1');
+    const { nextRoot } = nextRender();
+
+    expect(screen.getByText(loadingText)).toBeInTheDocument();
+
+    const loadingSnapshot = prettier.format(nextRoot.outerHTML, {
+      parser: 'html',
+    });
+    // snapshot作成・比較
+    expect(loadingSnapshot).toMatchSnapshot();
+
+    // コンポーネントの状態変更はactでラップする
+    // @see https://ja.reactjs.org/docs/test-utils.html#act
+    await act(async () => {
+      // 通信完了を待機
+      await waitForElementToBeRemoved(() => screen.getByText(loadingText));
     });
 
-    render();
-    // expect(screen.getByText('title foo')).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: 'Post Detail' })
+    ).toBeInTheDocument();
+
+    const loadedSnapshot = prettier.format(nextRoot.outerHTML, {
+      parser: 'html',
+    });
+    expect(loadedSnapshot).toMatchSnapshot();
   });
 });
